bad6bded2551615d429ae50155064ee173cd82bb
cheke15 <jesrt1579@gmail.com>
2023-11-27 17:27:13 -0600

    css

--- registro/principal.php
+++ registro/principal.php
@@ -6,71 +6,93 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar sesión</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
-    
+
     <style>
-        body {
-            background-color: wheat;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-        html {
-  font-size: 1em;
-}
-
-h1 {
-  font-size: 2rem;
-}
-
-@media only screen and (max-width: 768px) {
     body {
-        font-size: 16px;
-    }
-}
-@media only screen and (max-width: 480px) {
-    
-    body {
-        font-size: 14px;
-    }
-  }
-        form{
-            justify-content: center;
-            align-items: center;
-            padding: 75px;
-            border-radius: 30px; 
-            box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.1);
-            background-color: whitesmoke;
-        }
-        .fa{
+    background: linear-gradient(to bottom right, #f6a9ff, #e3a1ff, #cf99ff, #ba91ff, #a588ff, #927fff, #7e77ff, #6a6eff, #5566ff);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+     }
+
+    form {
+    font-family: "Homer Simpson UI";
+    font-size: 100%;
+    font-style: oblique;
+    border-radius: 15px;
+    box-shadow: 20px 10px 10px rgba(0.1, 0.2, 0.4, 0.8);
+    background-color: rgba(255, 255, 255, 0.7);
+    padding: 20px; 
+    width: 300px; 
+    max-width: 400px;
+    display: block; 
+      }
+
+   .logo-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 15px;
+      }
+
+    .logo {
+    width: 100px;
+     }
+    h1 {
+    text-align: center;
+    margin-bottom: 20px; 
+     }
+
+    form label,
+    form input {
+    display: block;
+    margin: 0px 0; 
+    width: 100%; 
+      }
+
+    button {
+    width: 100%;
+    margin-top: 07px; 
+      }
+   .register-link {
+    text-align: center;
+    display: flex;
+    margin-top: 0px;
+      }
+
+    button + .register-link {
+    margin-top: 0px;
+       }
+        .fa {
             display: none;
         }
     </style>
 </head>
 
-     <h1><div class="logo-container">
-        <img src="logo2.jpeg" alt="Logo de la empresa" class="logo">
-    </div></h1>
-    <form id="contenido" method="post" action="login2.js"> <!-- Ajusta el nombre del archivo PHP -->
-        <h2>Iniciar sesión</h2>
+<body>
+    
+
+    <form id="contenido" method="post" action="login2.js">
+    <div class="logo-container">
+        <img src="ab.jpeg" alt="Logo de la empresa" class="logo">
+    </div>
+        <h1>Iniciar sesión</h1> 
         <label for="nombredeusuario">Usuario:</label>
         <input type="text" id="nombredeusuario" name="nombredeusuario" required><br>
 
         <label for="contraseña">Contraseña:</label>
         <input type="password" id="contraseña" name="contraseña" required><br>
-       
-        <button id="btn" class="btn btn-info mt-3" type="submit" >Iniciar sesión</button><br> 
+
+        <button id="btn" class="btn btn-info mt-3" type="submit">Iniciar sesión</button><br>
         <i class="fa-regular fa-rotate-right fa-spin"></i>
         <br><a href="registro.php" class="register-link">¿No tienes una cuenta? ¡Regístrate aquí!</a>
-   </form>
+    </form>
 
-   <script
-  src="https://code.jquery.com/jquery-3.7.1.min.js"
-  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-  crossorigin="anonymous"></script>
-  <script src= "login2.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="login2.js"></script>
 </body>
 
 </html>
